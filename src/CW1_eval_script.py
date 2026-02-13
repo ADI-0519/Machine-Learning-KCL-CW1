f1dda2be92ef10050dcfce801a9d27286ac7a31b
Aditya Ranjan <adiranjan2019@gmail.com>
2026-02-13 13:05:27 +0000

    Added gitignore, requirements.txt and initial scaffold

--- src/CW1_eval_script.py
+++ src/CW1_eval_script.py
@@ -7,8 +7,8 @@ from sklearn.model_selection import train_test_split
 np.random.seed(123)
 
 # Import training data
-trn = pd.read_csv('CW1_train.csv')
-X_tst = pd.read_csv('CW1_test.csv') # This does not include true outcomes (obviously)
+trn = pd.read_csv('data/CW1_train.csv')
+X_tst = pd.read_csv('data/CW1_test.csv') # This does not include true outcomes (obviously)
 
 # Identify categorical columns
 categorical_cols = ['cut', 'color', 'clarity']
@@ -29,23 +29,7 @@ yhat_lm = model.predict(X_tst)
 # Format submission:
 # This is a single-column CSV with nothing but your predictions
 out = pd.DataFrame({'yhat': yhat_lm})
-out.to_csv('CW1_submission_KNUMBER.csv', index=False) # Please use your k-number here
-
-################################################################################
-
-# At test time, we will use the true outcomes
-tst = pd.read_csv('CW1_test_with_true_outcome.csv') # You do not have access to this
-
-# This is the R^2 function
-def r2_fn(yhat):
-    eps = y_tst - yhat
-    rss = np.sum(eps ** 2)
-    tss = np.sum((y_tst - y_tst.mean()) ** 2)
-    r2 = 1 - (rss / tss)
-    return r2
-
-# How does the linear model do?
-print(r2_fn(yhat_lm))
+out.to_csv('CW1_submission_23149795.csv', index=False) # k-no
 
 
 
